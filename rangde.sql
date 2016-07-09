42b99e7eeaf2e15a4f1addf5458856ade8d5a2d9
Anupam Singh <anupamsngh518@gmail.com>
2016-07-09 21:15:25 +0530

    Add files via upload

--- rangde.sql
+++ rangde.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 09, 2016 at 03:58 PM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Generation Time: Jul 09, 2016 at 04:38 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `rangde`
@@ -26,40 +26,40 @@ SET time_zone = "+00:00";
 -- Table structure for table `borrower`
 --
 
-CREATE TABLE IF NOT EXISTS `borrower` (
-  `first_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
-  `address` text NOT NULL,
-  `problem` text NOT NULL,
-  `group` varchar(200) NOT NULL,
-  `amount` int(11) NOT NULL,
-  `b_id` int(11) NOT NULL AUTO_INCREMENT,
-  `i_id` int(11) NOT NULL,
-  PRIMARY KEY (`b_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+CREATE TABLE `borrower` (
+  `BORROWER_ID` int(10) NOT NULL,
+  `BORROWER_NAME` varchar(50) DEFAULT NULL,
+  `BORROWER_ADDRESS` text,
+  `COMPLETE` int(1) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `borrower`
+--
+
+INSERT INTO `borrower` (`BORROWER_ID`, `BORROWER_NAME`, `BORROWER_ADDRESS`, `COMPLETE`) VALUES
+(1, 'ABC', 'XYZ', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `domain`
+-- Table structure for table `causes`
 --
 
-CREATE TABLE IF NOT EXISTS `domain` (
-  `group` varchar(100) NOT NULL,
-  `i_id` int(11) NOT NULL
+CREATE TABLE `causes` (
+  `CAUSE_ID` int(10) NOT NULL,
+  `BORROWER_ID` int(10) DEFAULT NULL,
+  `SECTION` varchar(50) DEFAULT NULL,
+  `AMOUNT` int(30) DEFAULT NULL,
+  `CAUSE_TITLE` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `domain`
+-- Dumping data for table `causes`
 --
 
-INSERT INTO `domain` (`group`, `i_id`) VALUES
-('education', 1),
-('education', 1),
-('education', 2),
-('education', 2),
-('finance', 2),
-('finance', 2);
+INSERT INTO `causes` (`CAUSE_ID`, `BORROWER_ID`, `SECTION`, `AMOUNT`, `CAUSE_TITLE`) VALUES
+(1, 1, 'EDUCTAION LOAN', 10000, 'STUDENT LOAN');
 
 -- --------------------------------------------------------
 
@@ -67,36 +67,92 @@ INSERT INTO `domain` (`group`, `i_id`) VALUES
 -- Table structure for table `investors`
 --
 
-CREATE TABLE IF NOT EXISTS `investors` (
-  `i_id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
-  `email_id` varchar(200) NOT NULL,
-  `phone` int(10) NOT NULL,
-  `rating` int(11) NOT NULL,
-  `time` date NOT NULL,
-  `facebook_id` varchar(200) NOT NULL,
-  PRIMARY KEY (`i_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `investors`
---
-
-INSERT INTO `investors` (`i_id`, `first_name`, `last_name`, `email_id`, `phone`, `rating`, `time`, `facebook_id`) VALUES
-(1, 'shankar', 'vanga', 'shankarvanga95@gmail.com', 96528159, 3, '2016-07-09', 'fb-id'),
-(2, 'u', '1', 'u1@gmail.com', 234532, 3, '2016-07-09', 'u1@fb.com');
+CREATE TABLE `investors` (
+  `ID` int(10) NOT NULL,
+  `USERNAME` varchar(50) DEFAULT NULL,
+  `PASSWRD` varchar(50) DEFAULT NULL,
+  `FIRST_NAME` varchar(50) DEFAULT NULL,
+  `LAST_NAME` varchar(50) DEFAULT NULL,
+  `ADDRESS` varchar(50) DEFAULT NULL,
+  `PHONE` varchar(50) DEFAULT NULL,
+  `FACEBOOK_URL` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `rating`
+-- Table structure for table `preferences`
 --
 
-CREATE TABLE IF NOT EXISTS `rating` (
-  `i_id` int(11) NOT NULL,
-  `rating` int(11) NOT NULL
+CREATE TABLE `preferences` (
+  `PREFERENCE_ID` int(10) DEFAULT NULL,
+  `ID` int(10) DEFAULT NULL,
+  `VAL` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `borrower`
+--
+ALTER TABLE `borrower`
+  ADD PRIMARY KEY (`BORROWER_ID`);
+
+--
+-- Indexes for table `causes`
+--
+ALTER TABLE `causes`
+  ADD PRIMARY KEY (`CAUSE_ID`),
+  ADD KEY `BORROWER_ID` (`BORROWER_ID`);
+
+--
+-- Indexes for table `investors`
+--
+ALTER TABLE `investors`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `preferences`
+--
+ALTER TABLE `preferences`
+  ADD KEY `ID` (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `borrower`
+--
+ALTER TABLE `borrower`
+  MODIFY `BORROWER_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `causes`
+--
+ALTER TABLE `causes`
+  MODIFY `CAUSE_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `investors`
+--
+ALTER TABLE `investors`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `causes`
+--
+ALTER TABLE `causes`
+  ADD CONSTRAINT `causes_ibfk_1` FOREIGN KEY (`BORROWER_ID`) REFERENCES `borrower` (`BORROWER_ID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `preferences`
+--
+ALTER TABLE `preferences`
+  ADD CONSTRAINT `preferences_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `investors` (`ID`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
