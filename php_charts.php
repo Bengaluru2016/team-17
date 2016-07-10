a4ac031742a4500a2761b5db73ea29c8a46d22ea
AAkhila <akki.achanta@gmail.com>
2016-07-09 21:57:30 -0500

    Chart on Statistical data of investors

--- php_charts.php
+++ php_charts.php
@@ -1,31 +1,22 @@
-<?php
-    
-    include "connection.php";
-    $sql = "select cause_title,count(borrower_id) as count1 from causes group by cause_title";
-    $result = $conn->query($sql);
-    session_destroy();
-?>
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
       google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawChart);
       function drawChart() {
 
         var data = google.visualization.arrayToDataTable([
-          ['Cause', 'Borrower'],
-        <?php
-            //$num = array(3,4);
-            for($i=0;$i<$result->num_rows-1;$i++){
-                $row = $result->fetch_assoc();
-               echo "['".$row['cause_title']."',".$row['count1']."],";
-            }
-                $row = $result->fetch_assoc();
-               echo "['".$row['cause_title']."',".$row['count1']."]";
-            ?>
+          ['Sectors', 'Investors'],
+          ['Education',     3],
+          ['Sanitary',      2],
+          ['Agriculture',  2],
+          ['Vacation', 1],
+          ['Business',    7]
         ]);
 
         var options = {
-          title: 'Statistical data of in investments in each '
+          title: 'Statistical Data of Investors'
         };
 
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
@@ -33,16 +24,8 @@
         chart.draw(data, options);
       }
     </script>
-    <body>
-        <?php
-//$x=$result->fetch_array(MYSQLI_NUM);
-        //echo $x[2];
-$result = $conn->query($sql);
-            for($i=0;$i<$result->num_rows;$i++){
-                $row = $result->fetch_assoc();?>
-        <?php echo $row['cause_title']; echo " ".$row['count1'];?><br>
-        <?php
-            }
-        ?>
-        <div id="piechart" class="col-xs-6 col-sm-6 col-md-6" style="width: 900px; height: 500px;"></div>
-</body>
+  </head>
+  <body>
+    <div id="piechart" style="width: 900px; height: 500px;"></div>
+  </body>
+</html>
